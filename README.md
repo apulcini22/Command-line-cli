e7b6e3af6b1d15099c3425cdef8c9fa256401c2a
Anthony Pulcini <pulcinianthony@gmail.com>
2019-07-02 10:08:37 -0700

    added more info to README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 This is a command line tool for managing customer information in a database. It uses Commander for dealing with terminal interactions, Inquirer for terminal prompts to obtain user input and Mongoose to interact with the database.
+
+How to use:
+1) Run customer-cli then a command name in terminal to execute.
+2) Run command --help for commands.
+
+Command names:
+add|a           Add a customer
+find|f <name>   Find a customer
+update|u <_id>  Update a customer
+list|l          List all customers
+remove|r <_id>  Remove a customer
--- commands.js
+++ commands.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const program = require('commander');
 const { prompt } = require('inquirer');
 const { 
@@ -51,14 +52,14 @@ program
 program 
   .command('find <name>')
   .alias('f')
-  .description('find customer')
+  .description('Find a customer')
   .action( (name) => findCustomer(name) );
 
 // Update a customer command
 program 
   .command('update <_id>')
   .alias('u')
-  .description('Update customer')
+  .description('Update a customer')
   .action( (_id) => {
     prompt(questions)
     .then(answers => updateCustomer(_id, answers) )
@@ -69,14 +70,14 @@ program
 program 
   .command('list')
   .alias('l')
-  .description('Customer list')
+  .description('List all customers')
   .action( () => listCustomers() )
 
 // Remove a customer command
 program 
   .command('remove <_id>')
   .alias('r')
-  .description('Removed Customer')
+  .description('Remove a customer')
   .action( (_id) => removeCustomer(_id) )
 
 
